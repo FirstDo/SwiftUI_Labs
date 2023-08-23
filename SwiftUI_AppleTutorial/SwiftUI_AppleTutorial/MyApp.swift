346b566be0bec4578e0277588c1e1f9a5808b035
dudu <whqtkf12@naver.com>
2023-08-23 13:29:19 +0900

    Composing Complex Interfaces

--- SwiftUI_AppleTutorial/SwiftUI_AppleTutorial/MyApp.swift
+++ SwiftUI_AppleTutorial/SwiftUI_AppleTutorial/MyApp.swift
@@ -3,11 +3,28 @@ import SwiftUI
 @main
 struct MyApp: App {
   @StateObject private var modelData = ModelData()
+  @State private var selection: Tab = .featured
+  
+  enum Tab {
+    case featured
+    case list
+  }
   
   var body: some Scene {
     WindowGroup {
-      LandmarkList()
-        .environmentObject(modelData)
+      TabView(selection: $selection) {
+        CategoryHome()
+          .tabItem {
+            Label("Featured", systemImage: "star")
+          }
+          .tag(Tab.featured)
+        LandmarkList()
+          .tabItem {
+            Label("List", systemImage: "list.bullet")
+          }
+          .tag(Tab.list)
+      }
+      .environmentObject(modelData)
     }
   }
 }
