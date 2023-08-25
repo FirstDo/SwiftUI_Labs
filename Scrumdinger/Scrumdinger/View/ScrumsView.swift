cf5be3fd8faaa5f8f9e826c7f9be7de9abe995c2
dudu <whqtkf12@naver.com>
2023-08-25 09:59:22 +0900

    Updating App Data

--- Scrumdinger/Scrumdinger/View/ScrumsView.swift
+++ Scrumdinger/Scrumdinger/View/ScrumsView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct ScrumsView: View {
   @Binding var scrums: [DailyScrum]
+  @State private var isPresentingNewScrumView = false
   
   var body: some View {
     NavigationStack {
@@ -14,12 +15,14 @@ struct ScrumsView: View {
       .navigationTitle("Daily Scrums")
       .toolbar {
         Button {
-          
+          isPresentingNewScrumView = true
         } label: {
           Image(systemName: "plus")
         }
         .accessibilityLabel("New Scrum")
-
+      }
+      .sheet(isPresented: $isPresentingNewScrumView) {
+         NewScrumSheet(scrums: $scrums, isPresentingNewScrumView: $isPresentingNewScrumView)
       }
     }
   }
