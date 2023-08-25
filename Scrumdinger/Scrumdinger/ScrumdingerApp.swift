2edaf50752ad5ca292437e03b74aa0b7a0123890
dudu <whqtkf12@naver.com>
2023-08-25 09:21:30 +0900

    Passing data with bindings

--- Scrumdinger/Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -2,9 +2,10 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  @State private var scrums = DailyScrum.sampleData
+  var body: some Scene {
+    WindowGroup {
+      ScrumsView(scrums: $scrums)
     }
+  }
 }
