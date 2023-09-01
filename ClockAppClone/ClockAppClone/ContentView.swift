1e3dacf2771f09df9defc6ddb98ff9a859856e2c
dudu <whqtkf12@naver.com>
2023-09-01 23:26:44 +0900

    TabView 뼈대 구현

--- ClockAppClone/ClockAppClone/ContentView.swift
+++ ClockAppClone/ClockAppClone/ContentView.swift
@@ -2,13 +2,29 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundColor(.accentColor)
-      Text("Hello, world!")
+    NavigationStack {
+      TabView {
+        WorldClockView()
+          .tabItem {
+            Label("세계 시계", systemImage: "globe")
+          }
+        AlarmView()
+          .tabItem {
+            Label("알람", systemImage: "alarm.fill")
+          }
+        StopWatchView()
+          .tabItem {
+            Label("스톱워치", systemImage: "stopwatch.fill")
+          }
+          .toolbar(.hidden, for: .navigationBar)
+        TimerView()
+          .tabItem {
+            Label("타이머", systemImage: "timer")
+          }
+          .toolbar(.hidden, for: .navigationBar)
+      }
+      .tint(.orange)
     }
-    .padding()
   }
 }
 
---
+++ ClockAppClone/ClockAppClone/WorldClockTab/WorldClockView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct WorldClockView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct WorldClockView_Previews: PreviewProvider {
+  static var previews: some View {
+    WorldClockView()
+  }
+}
