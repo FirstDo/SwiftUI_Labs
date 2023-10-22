b365c2b53d79dd791a5518212104e5a34737c567
dudu <whqtkf12@naver.com>
2023-10-22 22:19:23 +0900

    feat: Coordinator Router

---
+++ CoordinatorEX/CoordinatorEX/A/AView.swift
@@ -0,0 +1,69 @@
+//
+//  AView.swift
+//  CoordinatorEX
+//
+//  Created by LS-MAC-00213 on 2023/10/22.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ACore: Reducer {
+  struct State: Equatable {
+    
+  }
+  
+  let router: RouterImpl
+  
+  init(router: RouterImpl) {
+    self.router = router
+  }
+  
+  enum Action: Equatable {
+    case push
+    case modal
+    case pop
+  }
+  
+  func reduce(into state: inout State, action: Action) -> Effect<Action> {
+    switch action {
+    case .push:
+      router.pushA()
+    case .modal:
+      router.modalA()
+    case .pop:
+      router.pop()
+    }
+    return .none
+  }
+}
+
+struct AView: View {
+  let store: StoreOf<ACore>
+  
+  init(router: RouterImpl) {
+    self.store = .init(initialState: .init()) {
+      ACore(router: router)
+    }
+  }
+  
+  var body: some View {
+    Button("push") {
+      store.send(.push)
+    }
+    
+    Button("show") {
+      store.send(.modal)
+    }
+    
+    Button("pop") {
+      store.send(.pop)
+    }
+  }
+}
+
+extension View {
+  var toVC: UIViewController {
+    return UIHostingController(rootView: self)
+  }
+}
--- CoordinatorEX/CoordinatorEX/SceneDelegate.swift
+++ CoordinatorEX/CoordinatorEX/SceneDelegate.swift
@@ -8,6 +8,44 @@
 import UIKit
 import SwiftUI
 
+import ComposableArchitecture
+
+protocol Router: AnyObject {
+  var navi: UINavigationController? { get set }
+}
+
+class RouterImpl: Router {
+  weak var navi: UINavigationController?
+  
+  init(navi: UINavigationController?) {
+    self.navi = navi
+  }
+  
+  func pushA() {
+    navi?.pushViewController(AView(router: self).toVC, animated: true)
+  }
+  
+  func modalA() {
+    let nav = UINavigationController()
+    let router = RouterImpl(navi: nav)
+    let aView = AView(router: router).toVC
+    nav.setViewControllers([aView], animated: true)
+    navi?.topViewController?.present(nav, animated: true)
+  }
+  
+  func pop() {
+    navi?.popViewController(animated: true)
+  }
+  
+  func popToRoot() {
+    navi?.popToRootViewController(animated: true)
+  }
+  
+  deinit {
+    print(#function)
+  }
+}
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
@@ -16,27 +54,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     window = UIWindow(windowScene: windowScene)
   
-    let vc1 = UIViewController()
-    vc1.view.backgroundColor = .red
-  
-    window?.rootViewController = UINavigationController(rootViewController: vc1)
+    let navi = UINavigationController()
+    
+    let router = RouterImpl(navi: navi)
+    let vc = AView(router: router).toVC
+    
+    navi.setViewControllers([vc], animated: true)
+    
+    window?.rootViewController = navi
     window?.makeKeyAndVisible()
-        
-    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-      let navi = showModal(top: vc1)
-      
-      DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-        let navi = showModal(top: navi.topViewController!)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-          let navi = showModal(top: navi.topViewController!)
-          
-          DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            let navi = showModal(top: navi.topViewController!)
-          }
-        }
-      }
-    }
   }
 
   func sceneDidDisconnect(_ scene: UIScene) { }
