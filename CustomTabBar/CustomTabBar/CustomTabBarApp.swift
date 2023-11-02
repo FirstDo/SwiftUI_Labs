a9ab7303cb7dfd9584c2614828bcf8cf03780ad5
dudu <whqtkf12@naver.com>
2023-11-03 08:35:41 +0900

    setup: CustomTabBar Proj

---
+++ CustomTabBar/CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by LS-MAC-00213 on 2023/11/03.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
